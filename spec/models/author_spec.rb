49995d1250b15862087f8d37ee2a02e705215797
Tonybodo <arnold4@uni-potsdam.de>
2022-11-06 18:51:48 +0100

    Hotfix

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+    
+    before :each do
+        @author = Author.new(
+            first_name: "Alan",
+            last_name: "Turing",
+            homepage: "http://wikipedia.org/Alan_Turing")
+    end
+
     it "should have first name, last name, homepage and function to show whole name" do
-        before :each do
-            @author = Author.new(
-                first_name: "Alan",
-                last_name: "Turing",
-                homepage: "http://wikipedia.org/Alan_Turing")
-        end
-        
         expect(author.first_name).to eq(firstName)
         expect(author.last_name).to eq(lastName)
         expect(author.homepage).to eq(homepage)
